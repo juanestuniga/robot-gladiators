6cb63044b076da076528714f1a35a684cc86f236
JuanEstuniga <JuanCZuniga@Juans-MacBook.local>
2022-03-10 11:35:26 -0500

    added player's and enemy's health, attack and name. created fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,31 @@
-// Gets players name
 var playerName = window.prompt("what is your robot's name?");
-console.log(playerName);
-console.log(10+10);
-console.log("Our robot's name is " + playerName)
-// this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun!");
-}
-// fight();
+var playerHealth = 100;
+var playerAttack = 10;
+
+//You can also log multiple values at once like this 
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemeyAttack = 12;
+// Create function
+var fight = function() {
+     // Alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
+
+  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  enemyHealth = enemyHealth - playerAttack;
+
+  // Log a resulting message to the console so we know that it worked.
+
+  console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.")
+
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  playerHealth = playerHealth - enemeyAttack;
+
+  // Log a resulting message to the console so we know that it worked.
+  console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+};
+// Execute function
+fight();
